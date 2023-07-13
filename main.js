f463150eacc74052cfa7de58e99f1a77da7b2712
moon <cogus2010@naver.com>
2023-07-13 19:06:50 +0900

    chap2

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-import _ from "lodash"
+const name1 = ['병현이 index','채현이 index','진영이 index','윤수 index'];
 
-console.log('hello')
+name1.forEach((number,index) => {
+    console.log(number);
+    console.log(index);
+})
+
+
+const name2 = ['병현이','채현이','진영이','윤수']
+for(number of name2){
+	console.log(number);
+}
+
+
+const name3 = {
+    title:"신나는 자바스크립트~!",
+    member:"병현, 채현, 진영, 윤수"
+}
+for(key in name3){
+    console.log(`${key} : ${name3[key]}`)
+}
